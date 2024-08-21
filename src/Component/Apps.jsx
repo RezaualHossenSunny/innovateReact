1e16b845094411791e30310fca5041039104cd7c
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-08-22 00:18:48 +0600

    business responcive done

--- src/Component/Apps.jsx
+++ src/Component/Apps.jsx
@@ -7,19 +7,20 @@ import brand5 from '../assets/4.png'
 const Apps = () => {
   return (
     <div className='bg-[#F4FAFA]'>
-        <div className='max-w-container mx-auto py-8'>
-            <div className='py-8 flex justify-between'>
+        <div className='max-w-container mx-auto py-8  p-4 md:p-0 '>
+            <div className='py-8  md:flex justify-between'>
+              {/* 1st content */}
                 <div className='w-2/4'>
                 <p className='text-xl font-chirvo font-medium text-[#FF7364]'>Integrations</p>
 
-                <h1 className='font-chirvo font-bold  text-5xl text-second w-[587px]'>Easily integrate with your favorite apps</h1>
+                <h1 className='font-chirvo font-bold  text-3xl  md:text-5xl text-second  w-[330px] md:w-[587px]'>Easily integrate with your favorite apps</h1>
 
-                <p className='mt-4 font-chirvo font-medium text-lg text-gray w-[533px]'>App integration, in a general sense, is the process of bringing resources or capabilities from one application to another. As the world of apps continues to evolve, app integration is becoming expected in many contexts.</p>
+                <p className='mt-4 font-chirvo font-medium  text-sm md:text-lg text-gray  w-[330px] md:w-[533px]'>App integration, in a general sense, is the process of bringing resources or capabilities from one application to another. As the world of apps continues to evolve, app integration is becoming expected in many contexts.</p>
 
 
                 
-                <div className='py-4 px-10 border-2 bg-blue rounded-md w-[196px]  mt-[40px] border-white'>
-        <p className='text-white text-lg font-medium text-center'>Get Started</p>
+                <div className='py-4  px-6 md:px-10 border-2 bg-blue rounded-md w-[196px]  mt-[20px] md:mt-[40px] border-white'>
+        <p className='text-white  text-sm md:text-lg font-medium text-center'>Get Started</p>
 
 
         
@@ -29,14 +30,15 @@ const Apps = () => {
 
                 </div>
 
+                  {/* 2nd content */}
 
-                <div className='w-2/4'>
-                <div className='flex justify-between'>
+                <div className=' w-full md:w-2/4 mt-8 md:mt-0'>
+                <div className='grid grid-cols-2'>
                 <img  src={brand1}/>
                 <img src={brand2}/>
                 </div>
 
-                <div className='flex justify-between'>
+                <div className='grid  grid-cols-2 md:grid-cols-3 '>
                 
                 <img src={brand3}/>
                 <img src={brand4}/>
