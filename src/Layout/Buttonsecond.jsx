21ae2c3abafe4b6201f55116c7334a91703e3c0d
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-08-21 23:19:06 +0600

    navbar responcive done

--- src/Layout/Buttonsecond.jsx
+++ src/Layout/Buttonsecond.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Buttonsecond = () => {
   return (
-    <div className='py-4 px-10 hover:ease-in duration-300   hover:border-2 hover:bg-comon hover:rounded-md w-[165px]  '>
-    <p className='text-white text-lg font-medium text-center'>Login</p>
+    <div className='  py-2 md:py-4  px-4 md:px-10 hover:ease-in duration-300   hover:border-2 hover:bg-comon hover:rounded-md w-[130px] md: w-[165px]  '>
+    <p className='text-white  text-sm md:text-lg font-medium text-center'>Login</p>
 </div>
   )
 }
